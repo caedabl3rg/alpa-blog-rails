45c8313fa40dd514d3fb97b4bfbbcb058f2e3603
tuyul gundul ucul <caedabl3rg@gmail.com>
2021-09-15 00:35:59 +0700

    edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,20 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            flash[:notice] = " User data succesfully  updated"
+            redirect_to articles_path
+        else
+            render 'edit'
+        end
+    end
     
     def create
         @user = User.new(user_params)
@@ -13,6 +27,7 @@ class UsersController < ApplicationController
             render 'new'
         end
     end
+
 
     private 
     def user_params
